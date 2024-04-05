57fbb27f7aab4f2a00b323a4e751816047671110
RubyK01 <tomaskehoe4@gmail.com>
2024-04-05 03:32:52 +0100

    updated uploadProducts-files

--- routes/uploadProduct.js
+++ routes/uploadProduct.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var session = require('express-session');
 var mysql = require('mysql');
 var connection_details = require("../modules/connection_details");
-var upload = require('express-fileupload');
 var path = require('path');
 
 //Made by Ruby.
@@ -47,14 +46,10 @@ router.get('/', function(req, res, next) {
   });
 });
 
-//https://www.youtube.com/watch?v=hyJiNTFtQic&t=2s
-//Above is where I learned how to use express-fileupload
 router.post('/upload', async function(req, res, next){
   var customerID = req.session.userID;
   var productName = req.body.productName;
   var price = parseFloat(req.body.price);
-  let image;
-  let uploadPath;
 
   if(!req.files || Object.keys(req.files).length === 0){
     var errorMessage = "File not uploaded!";
@@ -62,7 +57,6 @@ router.post('/upload', async function(req, res, next){
     return res.redirect("/uploadProduct?error=" + encodedError);
   }
 
-  image = req.files.image;
 
   if(price <= 0){
     var errorMessage = "Price must be a positive value!";
@@ -75,16 +69,7 @@ router.post('/upload', async function(req, res, next){
     return res.redirect("/uploadProduct?error=" + encodedError);
   }
   else{
-    //Had an issue with  __dirname as it would start from the routes folder so I had
-    //figure out how to make it go back 1 level.
-    //https://dev.to/ayenyeinsan/how-to-go-back-directory-in-nodejs-gg3
-    let goBack = path.join(__dirname,'../');
-    uploadPath = goBack + "/public/userSubmittedProducts/" + image.name;
-    console.log(image);
-    image.mv(uploadPath);
-    var imageName = image.name
     connection.query("INSERT INTO products(productName, price, customerID) VALUES ((?),(?),(?));", [productName, price, customerID]);
-    connection.query("INSERT INTO productIMG(image, customerID) VALUES ((?),(?));", [imageName, customerID]);
     var message = "Product uploaded!";
     var encodedMessage = encodeURIComponent(message);
     return res.redirect("/uploadProduct?message=" + encodedMessage);
